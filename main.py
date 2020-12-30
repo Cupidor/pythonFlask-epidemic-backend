bf61ced8425a71f39a4f68c82b71682add9f67d7
魏传鑫 <wcxin@njgn.com>
2020-12-30 18:28:52 +0800

    爬虫

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # 1.导包
+import json
 from datetime import datetime
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import pymysql
+from bs4 import BeautifulSoup
+import requests
+from sqlalchemy import true
 
 pymysql.install_as_MySQLdb()
 # 2.创建flask应用对象
@@ -36,6 +40,33 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), unique=True)
     password = db.Column(db.String(64))
+
+
+# 中国疫情统计表
+class China(db.Model):
+    # 定义表名
+    __tablename__ = "china"
+    # 定义列对象
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lastUpdateTime = db.Column(db.DateTime, unique=True)
+    confirm = db.Column(db.Integer)
+    suspect = db.Column(db.Integer)
+    heal = db.Column(db.Integer)
+    dead = db.Column(db.Integer)
+
+
+# 地区疫情统计表
+class Area(db.Model):
+    # 定义表名
+    __tablename__ = "area"
+    # 定义列对象
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    lastUpdateTime = db.Column(db.DateTime, unique=True)
+    city = db.Column(db.String(64))
+    confirm = db.Column(db.Integer)
+    suspect = db.Column(db.Integer)
+    heal = db.Column(db.Integer)
+    dead = db.Column(db.Integer)
 
 
 # 知识表
@@ -149,9 +180,62 @@ def getNews():
         return
 
 
+# 爬虫接口数据
+@app.route('/getReal', methods=['POST', 'GET'])
+def getReal():
+    if request.method == 'POST':
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (XHTML, like Gecko) '
+                          'Chrome/73.0.3683.103 Safari/537.36 ',
+            'Content-Type': 'application/json'
+        }
+        url = 'https://www.ncovchina.com/data/getDisease.html'
+        res = requests.get(url, headers)
+        # res.encoding = "utf-8"
+        result = json.loads(res.text)
+        if result['ret'] == 0:
+            data = json.loads(result['data'])
+            lastUpdateTime = data['lastUpdateTime']
+            hasExist = China.query.filter(China.lastUpdateTime == lastUpdateTime).first()
+            if hasExist:
+                res = {
+                    'op': 'success',
+                    'data': data
+                }
+                return res
+            else:
+                confirm = data['chinaTotal']['confirm']
+                suspect = data['chinaTotal']['suspect']
+                heal = data['chinaTotal']['heal']
+                dead = data['chinaTotal']['dead']
+                info = China(lastUpdateTime=lastUpdateTime, confirm=confirm, suspect=suspect, heal=heal, dead=dead)
+                db.session.add(info)
+                db.session.commit()
+                areaTotal = data['areaTree'][0]['children']
+                for item in areaTotal:
+                    city = item['name']
+                    cityConfirm = item['total']['confirm']
+                    citySuspect = item['total']['suspect']
+                    cityHeal = item['total']['heal']
+                    cityDead = item['total']['dead']
+                    area = Area(lastUpdateTime=lastUpdateTime, city=city, confirm=cityConfirm, suspect=citySuspect, heal=cityHeal,
+                                dead=cityDead)
+                    db.session.add(area)
+                    db.session.commit()
+                res = {
+                    'op': 'success',
+                    'data': data
+                }
+                return res
+        else:
+            return
+    else:
+        return
+
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main():
+    return '后台启动成功'
 
 
 if __name__ == '__main__':
